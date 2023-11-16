023cfba53da627a3521ef40ec844358b1eb1911a
Nick H <82036636+28n@users.noreply.github.com>
2023-11-16 12:58:37 +0100

    yes 2

---
+++ fish/e0-conf/path.fish
@@ -0,0 +1,21 @@
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+set -gx PATH /opt/homebrew/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+    status --is-command-substitution; and return
+
+    if test -f .nvmrc; and test -r .nvmrc
+        nvm use
+    else
+    end
+end
