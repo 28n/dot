2323a82ba022ef43a34819f1562f98380200a9ef
Nick H <82036636+28n@users.noreply.github.com>
2024-01-15 12:07:22 +0100

    annual macbook commit

--- fish/e0-conf/aliases.fish
+++ fish/e0-conf/aliases.fish
@@ -5,3 +5,4 @@ alias nvim "sudo nvim"
 alias mkdir "mkdir -p"
 alias mkd "mkdir -p"
 alias r ranger
+alias cde0 "cd /Volumes/entr0pyeng1ne/"
--- fish/e0-conf/functions.fish
+++ fish/e0-conf/functions.fish
@@ -2,3 +2,20 @@ function mkcd
     mkdir $argv[1]
     cd $argv[1]
 end
+
+function cdj
+    set basepath /Volumes/entr0pyeng1ne/
+    set wildcard "*/*/$argv[1]*"
+    set fullpath "$basepath$wildcard"
+
+    if test -d "$fullpath"
+        cd "$fullpath"
+    else
+        set parent_dir (dirname "$basepath$wildcard:h")
+        if test -d "$parent_dir"
+            cd "$parent_dir"
+        else
+            echo "Error: Directory not found."
+        end
+    end
+end
